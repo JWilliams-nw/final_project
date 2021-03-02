40a1214636a954219f7cb0704d9957b9ddfa3fdf
Jason Williams <jason.williams@kellogg.northwestern.com>
2021-03-02 16:56:58 -0600

    firebase

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,10 +1,18 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyD4jYHyxTbajJlRUqfzEV1w7P8eHHOYYX4",
+  authDomain: "final-project-6dc13.firebaseapp.com",
+  projectId: "final-project-6dc13",
+  storageBucket: "final-project-6dc13.appspot.com",
+  messagingSenderId: "388947677610",
+  appId: "1:388947677610:web:81f2367175df424a229bd3"
+
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
 }
-//test
+
 module.exports = firebase
