c9a9dec3b48e7da8402a11baae1051dfe62962e4
Jason Williams <jason.williams@kellogg.northwestern.com>
2021-03-02 17:45:46 -0600

    updates

--- index.js
+++ index.js
@@ -7,6 +7,64 @@ firebase.auth().onAuthStateChanged(async function(user) {
       name: user.displayName,
       email: user.email
     })
+
+    document.querySelector('form').addEventListener('submit', async function(event) {
+      event.preventDefault()
+
+      let userclassesText = document.querySelector('#userclasses').value
+
+      if (userclassesText.length > 0) {
+        let docRef = await db.collection('userclasses').add({
+          text: userclassesText,
+          userId: user.uid
+        })
+
+        let userclassId = docRef.id
+        console.log(`new user class with ID ${userclassId} created`)
+
+        document.querySelector('.userclasses').insertAdjacentHTML('beforeend', `
+          <div class="userclasses-${userclassId} py-4 text-xl border-b-2 border-purple-500 w-full">
+            <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
+            ${userclassesText}
+          </div>
+        `)
+
+        document.querySelector(`.userclasses-${userclassId} .done`).addEventListener('click', async function(event) {
+          event.preventDefault()
+          document.querySelector(`.userclasses-${userclassId}`).classList.add('opacity-20')
+          await db.collection('userclasses').doc(userclassId).delete()
+        })
+        document.querySelector('#userclasses').value = ''
+      }
+    })
+
+    let querySnapshot = await db.collection('userclasses').where('userId', '==',user.uid).get()
+    console.log(`Number classes in collection: ${querySnapshot.size}`)
+
+    let userclasses = querySnapshot.docs
+    for (let i=0; i<userclasses.length; i++) {
+      let userclassid= userclasses[i].id
+      let userclass = userclasses[i].data()
+      let userclassText = userclass.text
+
+      document.querySelector('.userclasses').insertAdjacentHTML('beforeend', `
+        <div class="userclasses-${userclassid} py-4 text-xl border-b-2 border-purple-500 w-full">
+          <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
+          ${userclassText}
+        </div>
+      `)
+
+      document.querySelector(`.userclasses-${userclassid} .done`).addEventListener('click', async function(event) {
+        event.preventDefault()
+        document.querySelector(`.userclasses-${userclassid}`).classList.add('opacity-20')
+        await db.collection('userclasses').doc(userclassid).delete()
+      })
+    }
+
+
+
+
+
     document.querySelector('.sign-in-or-sign-out').innerHTML = `
       <button class="text-pink-500 underline sign-out">Sign Out</button>
     `
