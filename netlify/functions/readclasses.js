9ddf151ce997911d734da6609ca91cb9e68b4563
Jason Williams <jason.williams@kellogg.northwestern.com>
2021-03-10 20:01:13 -0600

    update

---
+++ netlify/functions/readclasses.js
@@ -0,0 +1,10 @@
+let firebase = require('./firebase')
+
+exports.handler = async function(event) {
+  let data = [] // sample only...
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  }
+}
+     
